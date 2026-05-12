4e183eb6655ff17f3de0862995a6ae5cd2432298
erick949 <73764245+erick949@users.noreply.github.com>
2026-05-11 21:19:41 -0600

    Modify Django settings for CORS and allowed hosts
    Updated Django settings to include new allowed host and CORS configuration.

--- servidor_mimishop/servidor_mimishop/settings.py
+++ servidor_mimishop/servidor_mimishop/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
     "mimishop-frontend.onrender.com",
+    "mimishop-backend.onrender.com", 
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -75,7 +76,9 @@ REST_FRAMEWORK = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # ← PRIMERO
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -83,9 +86,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'servidor_mimishop.urls'
